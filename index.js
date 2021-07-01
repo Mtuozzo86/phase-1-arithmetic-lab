4946c09f8fb06ffe3015e970cb355d9139fa20bd
Mtuozzo86 <Mtuozzo86@gmail.com>
2021-06-30 20:17:02 -0400

    commit tree ffc228b31e8891384b8b53a934ddbe511fac863c

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+console.log(multiply)
+
+const random = Math.floor(Math.random() * 10 + 1)
+
+
+const num3 = 12
+const num4 = 8
+const mod = num3 % num4
+console.log(mod)
+
+const max = Math.max(1, 5, 20)
